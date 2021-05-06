a817c3871f071d560114705484fc8e3d20cbf791
ym <2371579340@qq.com>
2021-05-06 15:38:43 +0800

    环境搭配

---
+++ shop-sso/src/main/java/com/xxxx/sso/SSOApplication.java
@@ -0,0 +1,24 @@
+package com.xxxx.sso;
+
+
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * SSO启动类
+ *
+ * @author zhoubin
+ * @since 1.0.0
+ */
+@SpringBootApplication
+//开启dubbo
+@EnableDubbo
+//扫描mapper接口包
+@MapperScan("com.ego.sso.mapper")
+public class SSOApplication {
+   public static void main(String[] args) {
+      SpringApplication.run(SSOApplication.class,args);
+   }
+}
